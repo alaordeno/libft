2c478564d30ab0478ba9ecd1118b203bbd5fe9f6
Alicia Laorden Odierna <alaorden@c2r9s4.42madrid.com>
2025-10-10 15:16:56 +0200

    42

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,11 +6,38 @@
 /*   By: alaorden <alaorden@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/08 16:36:24 by alaorden          #+#    #+#             */
-/*   Updated: 2025/10/08 16:42:38 by alaorden         ###   ########.fr       */
+/*   Updated: 2025/10/10 15:15:53 by alaorden         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
- size_t strlcat(char *dst, const char *src, size_t size)
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	unsigned int	c;
+	unsigned int	j;
+
+	c = 0;
+	j = 0;
+	while (dst[c] != '\0')
+		c++;
+	while (src[j] != '\0' && c < (size - 1))
+	{
+		dst[c] = src[j];
+		c++;
+		j++;
+	}
+	dst[c] = '\0';
+	return (c);
+}
+
+/*  int main(void)
  {
+	char	dst[] = "puta";
+	const char src[] = " funcion";
 	
+	ft_strlcat(dst, src, 15);
+	printf("%s\n", dst);
+	return (0);
  }
+  */
