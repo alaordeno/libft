dcfd2ab78f8d0eb2694c49863d980b793f7c1db2
Alicia Laorden Odierna <alaorden@c2r10s3.42madrid.com>
2025-10-10 13:17:10 +0200

    55

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,13 +6,13 @@
 /*   By: alaorden <alaorden@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/08 16:45:43 by alaorden          #+#    #+#             */
-/*   Updated: 2025/10/08 16:56:00 by alaorden         ###   ########.fr       */
+/*   Updated: 2025/10/10 13:16:37 by alaorden         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strchr(char *x, char y)
+char	*ft_strchr(const char *x, char y)
 {
 	int	i;
 
@@ -20,15 +20,21 @@ int	ft_strchr(char *x, char y)
 	while (x[i] != '\0')
 	{
 		if (x[i] == y)
-			return(i);
-		else 
+			return ((char *)&x[i]);
+		else
 			i++;
 	}
-	return (0);
+	return (NULL);
 }
-
+/* 
 int	main(void)
 {
+	char 	y;
+	char	x[] = "hola mundo";
 	
+	y = 'z';
+	printf("%s", ft_strchr(x, y));
+	return (0);
 }
-/*no esta acabdo*/
+ */
+//pasar norm
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: alaorden <alaorden@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/08 13:27:03 by alaorden          #+#    #+#             */
-/*   Updated: 2025/10/08 16:19:24 by alaorden         ###   ########.fr       */
+/*   Updated: 2025/10/10 13:06:38 by alaorden         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,5 +30,6 @@ size_t	ft_strlcpy(char *dest, char *src, size_t size);
 int		ft_strlen(char *x);
 char	ft_touper(char x);
 char	ft_tolower(char y);
+char	*ft_strchr(const char *x, char y);
 
 #endif 
